5eaf9200d85f895b5444665de705366b290ee0f0
Carlos Barçante <barcantecarlosdev@gmail.com>
2023-01-21 10:16:12 -0300

    feat: add routes to create habits and to recieve day information

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,14 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client'
+import appRoutes from './lib/routes';
 require('dotenv').config();
 
 
 const app = fastify();
-const prisma = new PrismaClient();
 const PORT: any = process.env.PORT;
 
-app.register(cors)
-
-app.get('/hello', async () => {
-    const day = await prisma.day.findMany();
-    return day;
-})
+app.register(cors);
+app.register(appRoutes);
 
 app.listen({
     port: PORT
